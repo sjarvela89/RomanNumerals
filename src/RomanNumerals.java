56a190ac10628745b8097c9159196c56721e480d
Sakari Järvelä <sjarvela89@hotmail.com>
2018-10-11 16:01:23 +0300

    Second test in progress. Fail guaranteed.

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -3,19 +3,34 @@ public class RomanNumerals {
 	public int convertToInteger(String romanNum) {
 		int returnValue=0;
 		// To be Implemented
-		if(romanNum.equals("I"))
+		if(romanNum.equals("I")||romanNum.equals("II")||romanNum.equals("III"))
 		{
-			returnValue=1;
+			returnValue=oneToThree(romanNum);
 		}
-		if(romanNum.equals("II"))
+		if(romanNum.equals("IV")||romanNum.equals("V")||romanNum.equals("VI")||romanNum.equals("VII")||romanNum.equals("VIII"))
 		{
-			returnValue=2;
-		}
-		if(romanNum.equals("III"))
-		{
-			returnValue=3;
+			returnValue=fourToEight(romanNum);
 		}
 		return returnValue;
 		
+	}
+	private int oneToThree(String romanNumOneToThree){
+		int returnOneToThree=0;
+		if(romanNumOneToThree.equals("I"))
+		{
+			returnOneToThree=1;
+		}
+		if(romanNumOneToThree.equals("II"))
+		{
+			returnOneToThree=2;
+		}
+		if(romanNumOneToThree.equals("III"))
+		{
+			returnOneToThree=3;
+		}
+		return returnOneToThree;
+	}
+	private int fourToEight(String romanNFTE){
+		return 1;
 	}
 }
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -55,5 +55,75 @@ public class TestRomanNumerals {
 			break;
 		}
 	}
+	@Test
+	public void testNumbersToEight() {
+		RomanNumerals testNumerals = new RomanNumerals();
+		scanString = new Scanner(System.in);
+		String numberString=scanString.next();
+		int i=0;
+		i = testNumerals.convertToInteger(numberString);
+		System.out.print("The value of the i is: "+i);
+		switch(i)
+		{
+			case 4:
+			{
+				if(numberString.equals("IV"))
+				{
+					break;
+				}
+				else
+				{
+					fail("Number incorrect");
+				}
+			}
+			case 5:
+			{
+				if(numberString.equals("V"))
+				{
+					break;
+				}
+				else
+				{
+					fail("Number incorrect");
+				}
+			}
+			case 6:
+			{
+				if(numberString.equals("VI"))
+				{
+					break;
+				}
+				else
+				{
+					fail("Number incorrect");
+				}
+			}
+			case 7:
+			{
+				if(numberString.equals("VII"))
+				{
+					break;
+				}
+				else
+				{
+					fail("Number incorrect");
+				}
+			}
+			case 8:
+			{
+				if(numberString.equals("VIII"))
+				{
+					break;
+				}
+				else
+				{
+					fail("Number incorrect");
+				}
+			}
+			default:
+				fail("Unexpexted something");
+			break;
+		}
+	}
 
 }
