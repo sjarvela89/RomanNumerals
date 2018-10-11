90a587f43f2c3599c014284317ae0826e8f18181
Sakari Järvelä <sjarvela89@hotmail.com>
2018-10-11 15:13:36 +0300

    First test case created TDD way.

--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -1,12 +1,58 @@
-import static org.junit.Assert.*;
+import java.util.Scanner;
 
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestRomanNumerals {
 
+	private Scanner scanString;
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testNumbersToThree() {
+		RomanNumerals testNumerals = new RomanNumerals();
+		scanString = new Scanner(System.in);
+		String numberString=scanString.nextLine();
+		int i = testNumerals.convertToInteger(numberString);
+		System.out.print("The value of the i is: "+i);
+		switch(i)
+		{
+			case 1:
+			{
+				if(numberString=="I")
+				{
+					break;
+				}
+				else
+				{
+					fail("Number incorrect");
+				}
+			}
+			case 2:
+			{
+				if(numberString=="II")
+				{
+					break;
+				}
+				else
+				{
+					fail("Number incorrect");
+				}
+			}
+			case 3:
+			{
+				if(numberString=="III")
+				{
+					break;
+				}
+				else
+				{
+					fail("Number incorrect");
+				}
+			}
+			default:
+				fail("Unexpexted something");
+			break;
+		}
 	}
 
 }
